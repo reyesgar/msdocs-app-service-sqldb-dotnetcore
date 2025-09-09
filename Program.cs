ca82eeb8e110e5c72ca81457e7df6b69a6c6d0eb
reyesgar <66829791+reyesgar@users.noreply.github.com>
2025-09-09 12:38:08 +0000

    configure azure database and cache connections

--- Program.cs
+++ Program.cs
@@ -8,17 +8,25 @@ if(builder.Environment.IsDevelopment())
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
     builder.Services.AddDistributedMemoryCache();
-}
 // else
-// {
 //     builder.Services.AddDbContext<MyDatabaseContext>(options =>
-//         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 //     builder.Services.AddStackExchangeRedisCache(options =>
 //     {
 //     options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
 //     options.InstanceName = "SampleInstance";
 //     });
 // }
+    }
+    else
+    {
+        builder.Services.AddDbContext<MyDatabaseContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+        builder.Services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
+            options.InstanceName = "SampleInstance";
+        });
+    }
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
